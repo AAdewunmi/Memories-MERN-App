7be48251ec811a864b20f22929722d762f50da4a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-16 13:24:39 +0000

    Tenth Commit: Implement express() functionality on server side

--- server/index.js
+++ server/index.js
@@ -3,3 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+app.use(bodyParser.json({limit: "30mb", extend: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extend: true }));
+app.use(cors());
