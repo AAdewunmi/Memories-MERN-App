60a7a29745b3f6129f2bdcc1465240b46bc3758d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-18 11:39:53 +0000

    Twenty Commit: Refactor createPost to async function

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -9,6 +9,13 @@ export const getPost = async (req, res) => {
   }
 };
 
-export const createPost = (req, res) => {
-  res.send('Post Creation');
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
