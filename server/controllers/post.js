94250fb944426378e1e89341723923c9d4e3437e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-18 11:05:32 +0000

    Nineteen Commit: Refactor getPost() to async function

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,7 +1,12 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPost = (req, res) => {
-  res.send('This Works!');
+export const getPost = async (req, res) => {
+  try {
+      const postMessages = await PostMessage.find();
+      res.status(200).json(postMessages)
+  } catch (error) {
+      res.status(404).json({ message: error.message});
+  }
 };
 
 export const createPost = (req, res) => {
