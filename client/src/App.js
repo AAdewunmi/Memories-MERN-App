867f148436abdf7fcfa734b400cd6a4f58efb196
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-22 09:27:26 +0000

    Twenty-Nine Commit: Implement AppBar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
-
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memories from './images/memories.png';
 
 const App = () => {
     return (
-        
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Memories
+          </Typography>
+          <img src={memories} alt="memeries" height="60" />
+        </AppBar>
+      </Container>
     );
 }
 
