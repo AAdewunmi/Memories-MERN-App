e65c454388106ee958c47a4859e97007e3566b22
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-22 09:19:49 +0000

    Twenty-Eight Commit: Add memories.png to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+
 
 const App = () => {
     return (
-        <div>
-            <h1>
-                App!
-            </h1>
-        </div>
+        
     );
 }
 
