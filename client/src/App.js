9fc2fa01805f3679eb217f34f263689779c73d14
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-23 14:50:36 +0000

    Thirty-Six Commit: Implement styles for AppBar

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,39 @@ import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
+import useStyles from './styles.js';
 
 const App = () => {
+    const classes = useStyles();
     return (
       <Container maxWidth="lg">
-        <AppBar position="static" color="inherit">
-          <Typography variant="h2" align="center">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
             Memories
           </Typography>
-          <img src={memories} alt="memeries" height="60" />
+          <img
+            className={classes.image}
+            src={memories}
+            alt="memeries"
+            height="60"
+          />
         </AppBar>
         <Grow in>
-            <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                      <Posts />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                       <Form />
-                    </Grid>
-                </Grid>
-            </Container>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
         </Grow>
       </Container>
     );
