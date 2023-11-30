65103d56bd917ab8427ae5a99af372a39466ece9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-30 13:59:37 +0000

    Forty-Eight Commit: Implement wrapping App with provider component

--- client/src/index.js
+++ client/src/index.js
@@ -9,4 +9,9 @@ import App from './App';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
