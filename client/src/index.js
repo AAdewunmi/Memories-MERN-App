5ec81046b5471b93263dd68af8593449437ba8f4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-30 13:30:09 +0000

    Forty-Five Commit: Implement createStore() function

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+import App from './App';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(<App />, document.getElementById('root'));
