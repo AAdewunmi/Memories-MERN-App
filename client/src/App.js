8ca70d2ed307869f17a567a0bfd037a110e5a9d3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-11-30 14:11:19 +0000

    Forty-Nine Commit: Implement dispatch action

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import memories from './images/memories.png';
+import {useDispatch} from 'react-redux';
+import {getPosts} from './actions/posts.js'
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
+import memories from "./images/memories.png";
 import useStyles from './styles.js';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(getPosts());
+    }, [dispatch]);
     return (
       <Container maxWidth="lg">
         <AppBar className={classes.appBar} position="static" color="inherit">
