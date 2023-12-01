720b68da82ecdbcdb6d2b2408bf0db382c418f15
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-01 12:43:53 +0000

    Fifty-One Commit: Implement Redux functionality

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from "./styles.js";
 
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+  console.log(posts);
   return (
       <><h1>POSTS</h1><Post /><Post /></>
   );
