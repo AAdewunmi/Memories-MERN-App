9749cddf9b0dac6363c2c63fb7eea3f4c726b0f4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-01 12:53:50 +0000

    Fifty-Two Commit: Fix Error "access from origin has been blocked by cors policy"

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js'
 
 const app = express();
-
+app.use(cors());
 app.use('/posts', postRoutes);
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 
 const CONNECTION_URL =
   "mongodb+srv://Cluster24106:Obantoko101@cluster24106.bscljrg.mongodb.net/";
