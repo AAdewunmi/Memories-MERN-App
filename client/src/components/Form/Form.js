a84ff860b5a41422986ed831d409e86465bc847c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-02 16:06:26 +0000

    Fifty-Three Commit: Implement Paper element

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {TextField, Button, Typography, Paper} from '@material-ui/core';
 import useStyles from "./styles.js";
 
 const Form = () => {
     const classes = useStyles();
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            
+        </Paper>
     );
 }
 
