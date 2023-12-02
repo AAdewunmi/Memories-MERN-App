f80da680a8562c7510b05ce9fddba14e0a439aa6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-02 16:12:39 +0000

    Fifty-Five Commit: Implement skeleton handleSubmit() function

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,6 +4,9 @@ import useStyles from "./styles.js";
 
 const Form = () => {
     const classes = useStyles();
+    const handleSubmit = () => {
+
+    };
     return (
         <Paper className={classes.paper}>
             <Form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
