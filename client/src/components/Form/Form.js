ffc84738bf517a96a68c037b8bacfa53e7aadac3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-03 08:13:22 +0000

    Sixty-Two Commit: Implement Button element

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -71,6 +71,25 @@ const Form = () => {
               }
             />
           </div>
+          <Button
+            className={classes.buttonSubmit}
+            variant="contained"
+            color="primary"
+            size="large"
+            type="submit"
+            fullWidth
+          >
+            Submit
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="small"
+            onClick={clear}
+            fullWidth
+          >
+            Clear
+          </Button>
         </Form>
       </Paper>
     );
