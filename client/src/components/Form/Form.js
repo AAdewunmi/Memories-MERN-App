5204d1daa1d17aa3a5d7c2dc1f9ccdfa128f9a7a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-03 08:02:38 +0000

    Sixty Commit: Implement TextField element

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {TextField, Button, Typography, Paper} from '@material-ui/core';
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import useStyles from "./styles.js";
 
 const Form = () => {
@@ -7,30 +7,62 @@ const Form = () => {
         creator: '',
         title: '',
         message: '',
-        tags:'',
-        selectedFile:''
+        tags: '',
+        selectedFile: ''
     });
     const classes = useStyles();
     const handleSubmit = () => {
 
     };
     return (
-        <Paper className={classes.paper}>
-            <Form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
-                <Typography variant="h6">Creating a Memory</Typography>
-                    <TextField 
-                        name="creator" 
-                        variant="outlined" 
-                        label="Creator" 
-                        fullWidth 
-                        value={postData.creator} 
-                        onChange={
-                            (e) => setPostData({
-                                ...postData, creator: e.target.value
-                            })}
-                    />
-            </Form>
-        </Paper>
+      <Paper className={classes.paper}>
+        <Form
+          autoComplete="off"
+          noValidate
+          className={classes.form}
+          onSubmit={handleSubmit}
+        >
+          <Typography variant="h6">Creating a Memory</Typography>
+          <TextField
+            name="creator"
+            variant="outlined"
+            label="Creator"
+            fullWidth
+            value={postData.creator}
+            onChange={(e) =>
+              setPostData({ ...postData, creator: e.target.value })
+            }
+          />
+          <TextField
+            name="title"
+            variant="outlined"
+            label="Title"
+            fullWidth
+            value={postData.title}
+            onChange={(e) =>
+              setPostData({ ...postData, title: e.target.value })
+            }
+          />
+          <TextField
+            name="message"
+            variant="outlined"
+            label="Message"
+            fullWidth
+            value={postData.message}
+            onChange={(e) =>
+              setPostData({ ...postData, message: e.target.value })
+            }
+          />
+          <TextField
+            name="tags"
+            variant="outlined"
+            label="Tags"
+            fullWidth
+            value={postData.tags}
+            onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+          />
+        </Form>
+      </Paper>
     );
 }
 
