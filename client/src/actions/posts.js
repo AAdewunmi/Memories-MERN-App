b31ba6fc8f55f1f276a5ccab6b5f915989fcd01c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-06 13:15:51 +0000

    Sixty-Seven Commit: Implement createPost() function in actions/posts

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -9,3 +9,12 @@ export const getPosts = () => async (dispatch) => {
   }
 };
 
+export const createPost = (post) => (dispatch) => {
+  try {
+    const {data} = await api.createPost(post);
+    dispatch({ type: "CREATE", payload: data});
+  } catch (error) {
+    console.log(error.message)
+  }
+};
+
