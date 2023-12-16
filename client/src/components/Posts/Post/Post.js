aab9e9f518bef8baf69c0a8ae37a4e5b5d2d483e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-16 11:51:25 +0000

    Seventy-Three Commit: Implement Card element

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,18 @@
 import React from "react";
+import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import useStyles from "./styles.js";
 
 
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <h1>POST</h1>;
+  return (
+    <Card className={classes.card}>
+        <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia>
+    </Card>
+  );
 };
 
 export default Post;
