92e701bb82b01d4e928c518bde4fe42f493b8961
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-16 11:14:13 +0000

    Seventy-Two Commit: Implement Map object in Posts() function

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -12,7 +12,11 @@ const Posts = () => {
   return (
       !posts.length ? <CircularProgress></CircularProgress>:(
           <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-              
+              {posts.map((post) => (
+                <Grid key={post._id} item xs={12} sm={6}>
+                    <Post post={post}/>
+                </Grid>
+              ))}
           </Grid>
       ));
 };
