2e69b820200cf78121acf1d84c7396a565ac57c6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-25 10:39:16 +0000

    Eighty-Four Commit: Refactor project to use configureStore() instead of createStore()

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose} from "redux";
-import thunk from "redux-thunk";
+// import { createStore, applyMiddleware, compose} from "redux";
+// import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import reducers from "./reducers";
 import App from "./App";
 import "./index.css";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-
+// const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = configureStore({ reducer: reducers });
 ReactDOM.render(
     <Provider store={store}>
        <App />
