2e45bfda0747fdb3c8d9202177d7b37c19670855
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-07 09:49:23 +0000

    Ninety-Three Commit: Refactor updatePost() function to spread post

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -25,6 +25,6 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
   if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id!');
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
   res.json(updatedPost);
 }
