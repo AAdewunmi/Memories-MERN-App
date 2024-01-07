f56546a4d9924d9a4ade11ffa8fd102154c1375f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-07 13:37:47 +0000

    Ninety-Eight Commit: Implement deletePost() function insrc/actions/post.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -27,3 +27,12 @@ export const updatePost = (id, post) => async (dispatch) => {
   }
 };
 
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+    dispatch({ type: 'DELETE', payload: id});
+  } catch (error) {
+     console.log("Delete Post Error: " + error);
+  }
+}
+
