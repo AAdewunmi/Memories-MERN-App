0a75a6695a9e8e108471b3c987bb53634f374c09
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-08 10:15:08 +0000

    One-Hundred-Four Commit: Implement likePost() function in client/src/actions/post.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -36,3 +36,13 @@ export const deletePost = (id) => async (dispatch) => {
   }
 }
 
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+    dispatch({ type: 'UPDATE', payload: data });
+  } catch (error) {
+    console.log("Like Post Error: " + error);
+  }
+}
+
+
