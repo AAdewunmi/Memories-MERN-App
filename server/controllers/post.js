c344e227a4900007514ee84dcc543c9304039289
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-08 09:59:08 +0000

    One-Hundred-Two Commit: Implement likePost() function in server/controllers.post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -38,3 +38,12 @@ export const deletePost = async (req, res) => {
   await PostMessage.findByIdAndDelete(id);
   res.json({ message: "Post deleted successfully." });
 }
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, {new: true});
+  res.json(updatedPost);
+}
