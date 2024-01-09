a168e98df6f848c859e677ecc67e4c4b38c7a5f5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-09 10:39:31 +0000

    One Hundred-Sixteen Commit: untrack .env file

--- server/index.js
+++ server/index.js
@@ -2,18 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import postRoutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
+import 'dotenv/config';
 
 const app = express();
-
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://Cluster24106:Obantoko101@cluster24106.bscljrg.mongodb.net/";
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
+
