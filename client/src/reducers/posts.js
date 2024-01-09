8229245c51c26d6435335b2f468627d1a8d33965
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-09 09:28:07 +0000

    One-Hundred-Twelve Commit: Refactor reducers to use action type constants

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,16 +1,16 @@
+import { CREATE, UPDATE, DELETE, FETCH_ALL } from "../constants/actionTypes";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
     switch (action.type) {
-      case "FETCH_ALL":
+      case FETCH_ALL:
         return action.payload;
-      case "CREATE":
+      case CREATE:
         return [...posts, action.payload];
-      case "UPDATE":
-      case "LIKE":
+      case UPDATE:
         return posts.map((post) =>
           post._id === action.payload._id ? action.payload : post
         );
-      case "DELETE":
+      case DELETE:
         return posts.filter((post) => post._id !== action.payload);
       default:
         return posts;
