5ce8a76b5b59e3d574434295d6523098fe91b7e6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-10 10:40:20 +0000

    One-Hundred-Nineteen Commit: Implement Navbar as a component

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { AppBar, Typography } from "@material-ui/core";
+import memories from "../../images/memeries.png";
+import useStyles from "./styles";
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memeries"
+          height="60"
+        />
+      </AppBar>
+    );
+}
+export default Navbar;
