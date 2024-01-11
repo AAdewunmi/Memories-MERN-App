dd20046899ae568601d4863e0a687877308b24e3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-11 15:09:14 +0000

    One Hundred-Twenty-Two Commit: Implement Toolbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,20 +1,39 @@
 import React from "react";
-import { AppBar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { AppBar, Avatar, Toolbar, Typography } from "@material-ui/core";
 import memories from "../../images/memories.png";
 import useStyles from "./styles";
 const Navbar = () => {
     const classes = useStyles();
+    const user = null;
     return (
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="memeries"
-          height="60"
-        />
+        <div className={classes.brandContainer}>
+          <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
+            Memories
+          </Typography>
+          <img
+            className={classes.image}
+            src={memories}
+            alt="memeries"
+            height="60"
+          />
+        </div>
+        <Toolbar className={classes.toolbar}>
+            { user ? (
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+                        {user.result.name.charAt(0)}
+                    </Avatar>
+                    <Typography className={classes.userName} variant="h6">
+                        {user.result.name}
+                    </Typography>
+                    
+                </div>
+            ) : (
+
+            )}
+        </Toolbar>
       </AppBar>
     );
 }
