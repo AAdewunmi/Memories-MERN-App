673eba8f3dff4731aff702453e91b125b865d0cd
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-16 10:41:15 +0000

    One-Hundred-Thirty Commit: Create skeleton Input.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,15 @@
 import React from "react";
+import {Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import makeStyles from './styles';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 const Auth = () => {
-    const state = null;
-    return (  
-        <div>
-           AUTH
-        </div>
+    const classes = makeStyles();
+    const isSignUp = false;
+    const handleSubmit = () => {}
+    const handleChange = () => {};
+    return (
+     
     );
 }
 
