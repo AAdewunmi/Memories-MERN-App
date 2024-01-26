74efe97ede95037a7cf7f8ae3ce840731fc777f0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-26 16:52:41 +0000

    One-Hundred-Forty-Eight Commit: Create skeleton signIn() method src/actions/auth.js

---
+++ client/src/actions/auth.js
@@ -0,0 +1,11 @@
+import { AUTH } from "../constants/actionTypes";
+import * as api from "../api/index.js";
+
+export const signIn = (formData, history) => async (dispatch) => {
+  try {
+    history.push('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
