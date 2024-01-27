2a4acd449b188ebb357ea9c49931519507bd0d44
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-27 13:41:09 +0000

    One-Hundred-Fifty-Six  Commit: Implement signUp() controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -19,8 +19,27 @@ export const signIn = async(req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong!'});
     }
-}
+};
 
 export const signUp = async (req, res) => {
-
+    const { email, password, confirmedPassword, firstName, lastName } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ message: "User already exists." });
+        }
+        if (password != confirmedPassword) {
+            return res.status(400).json({ message: "Passwords don't match." });
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
+        const token = jwt.sign(
+          { email: result.email, id: result._id },
+          "test",
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ result, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong!" });
+    }
 };
