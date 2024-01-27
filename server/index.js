b6c757c87c16b823283a82b5c8dfb78a7fada31d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-27 12:25:05 +0000

    One-Hundred-Fifty Commit: Implement user routes in server/index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import userRoutes from "./routes/users.js";
 import 'dotenv/config';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL)
