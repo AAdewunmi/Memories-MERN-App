9ae85d2b86779538e03c13ce52a10b1bf54bdac1
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-31 16:21:27 +0000

    One-Hundred-Sixty-Nine Commit: Refactor siginout out functionaity

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -15,7 +15,7 @@ export const signup = (formData, history) => async (dispatch) => {
   try {
     const { data } = await api.signUp(formData);
     dispatch({ type: AUTH, data });
-    history.push('/');
+    history('/');
   } catch (error) {
     console.log(error);
   }
