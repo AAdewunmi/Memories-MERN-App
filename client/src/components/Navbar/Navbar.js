7ae2798da2a8a863bfc0a869775ec0f3abe3bb99
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-31 16:54:27 +0000

    One-Hundred-Seventy Commit: Refactor NavBar to display user name

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -43,17 +43,17 @@ const Navbar = () => {
           />
         </div>
         <Toolbar className={classes.toolbar}>
-          {user ? (
+          {user?.result ? (
             <div className={classes.profile}>
               <Avatar
                 className={classes.purple}
-                alt={user.name}
-                src={user.imageUrl}
+                alt={user?.result.name}
+                src={user?.result.imageUrl}
               >
-                {user.name}
+                {user?.result.name.charAt(0)}
               </Avatar>
               <Typography className={classes.userName} variant="h6">
-                {user.name}
+                {user?.result.name}
               </Typography>
               <Button
                 variant="contained"
