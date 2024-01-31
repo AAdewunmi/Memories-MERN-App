7ff8b65306681414705d6997405e7cf127bb7c7f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-31 10:07:49 +0000

    One-Hundred-Sixty-Four Commit: Implement signIn routes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,3 +8,5 @@ export const createPost = (newPost) => API.post('/posts', newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatePost) => API.patch(`/posts/${id}`, updatePost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
+
+export const signIn = (formData) => API.post('/users/signin', formData);
