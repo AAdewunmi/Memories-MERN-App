c2ecea49741c77c7472680f9f38e04ab836b2b16
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-18 12:07:38 +0000

    One-Hundred-Eighty-Nine Commit: Implement Pagination logic by redirecting posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
@@ -8,10 +8,13 @@ import Auth from "./components/Auth/Auth";
 const App = () => {
     return (
       <BrowserRouter>
-        <Container maxWidth="lg">
+        <Container maxWidth="xl">
           <Navbar />
           <Routes>
-            <Route path="/" exact Component={Home} />
+            <Route path="/" exact Component={() => <Redirect to="/posts" />} />
+            <Route path="/posts" exact Component={Home} />
+            <Route path="/posts/search" exact Component={Home} />
+            <Route path="/posts/id" exact Component={PostDetails} />
             <Route path="/auth" exact Component={Auth} />
           </Routes>
         </Container>
