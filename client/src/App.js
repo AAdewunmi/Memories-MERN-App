7a9f3be760c5a15cbd80b351da7ddba6dcd57e3c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-18 12:28:34 +0000

    One-Hundred-Ninety-Two Commit: Fix error "export 'Redirect' (imported as 'Redirect') was not found in 'react-router-dom'"

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
-
+import PostDetails from "./components/PostDetails/PostDetails";
 const App = () => {
     return (
       <BrowserRouter>
         <Container maxWidth="xl">
           <Navbar />
           <Routes>
-            <Route path="/" exact Component={() => <Redirect to="/posts" />} />
+            <Route path="/" exact Component={() => <Navigate to="/posts" />} />
             <Route path="/posts" exact Component={Home} />
             <Route path="/posts/search" exact Component={Home} />
-            <Route path="/posts/id" exact Component={PostDetails} />
+            <Route path="/posts/:id" exact Component={PostDetails} />
             <Route path="/auth" exact Component={Auth} />
           </Routes>
         </Container>
--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PostDetails = () => {
-  console.log('POST DETAILS');
+  console.log("POST DETAILS");
   return (
     <div>POST DETAILS</div>
   );
