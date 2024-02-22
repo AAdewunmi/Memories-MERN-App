613ef117584cf6afce8f43f89fe92c88f83792b2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-22 13:05:14 +0000

    One-Hundred-Ninety-Four Commit: Implement useQuery() function

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from "@material-ui/core";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 import Paginate from "../Pagination";
+import { useHistory, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
+
+function useQuery(){
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
      const [currentId, setCurrentId] = useState(0);
      const dispatch = useDispatch();
+     const query = useQuery();
+     const history = useHistory();
+     const page = query.get('page') || 1;
+     const searchQuery = query.get('searchQuery');
      useEffect(() => {
        dispatch(getPosts());
      }, [currentId, dispatch]);
