c2352ee29ed76765bd76e295ea9125a66906c4de
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-22 13:13:18 +0000

    One-Hundred-Ninety-Five Commit: Implement AppBar component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -24,21 +24,27 @@ const Home = () => {
      }, [currentId, dispatch]);
     return (
       <Grow in>
-        <Container>
+        <Container maxWidth="xl">
           <Grid
             container
             justifyContent="space-between"
             alignItems="stretch"
             spacing={3}
+            className={classes.gridContainer}
           >
-            <Grid item xs={12} sm={7}>
+            <Grid item xs={12} md={9} sm={6}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} md={3} sm={6}>
+              <AppBar
+                className={classes.appBarSearch}
+                position="static"
+                color="inherit"
+              ></AppBar>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
-                <Paper elevation={6}>
-                  <Paginate />
-                </Paper>
+              <Paper elevation={6}>
+                <Paginate />
+              </Paper>
             </Grid>
           </Grid>
         </Container>
