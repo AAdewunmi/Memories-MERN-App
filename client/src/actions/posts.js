8409f86d406bfea133de6cf611a434546452c33f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-02-26 07:29:55 +0000

    Two-Hundred-Twelve Commit: Refactor getPostsSearch() function

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -12,7 +12,7 @@ export const getPosts = () => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPostsBySearch(searchQuery);
+    const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
     console.log(data);
   } catch (error) {
     console.log(error);
