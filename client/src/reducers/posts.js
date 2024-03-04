9af0715fdc6a83c24bee11e97190e10bc963486f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-04 12:16:15 +0000

    Two-Hundred-Thirty-One Commit: Refactor reducers

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,6 @@
-import { CREATE, UPDATE, DELETE, FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, END_LOADING } from "../constants/actionTypes";
+import { CREATE, UPDATE, DELETE, FETCH_ALL, FETCH_BY_SEARCH, LIKE, START_LOADING, END_LOADING } from "../constants/actionTypes";
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = [], action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
     switch (action.type) {
       case START_LOADING:
         return { ...state, isLoading: true };
@@ -15,14 +15,27 @@ export default (state = [], action) => {
         };
       case FETCH_BY_SEARCH:
         return { ...state, posts: action.payload };
+      case LIKE:
+        return {
+          ...state,
+          posts: state.posts.map((post) =>
+            post._id === action.payload._id ? action.payload : post
+          ),
+        };
       case CREATE:
-        return [...state, action.payload];
+        return { ...state, posts: [...state.posts, action.payload] };
       case UPDATE:
-        return state.map((post) =>
-          post._id === action.payload._id ? action.payload : post
-        );
+        return {
+          ...state,
+          posts: state.posts.map((post) =>
+            post._id === action.payload._id ? action.payload : post
+          ),
+        };
       case DELETE:
-        return state.filter((post) => post._id !== action.payload);
+       return {
+         ...state,
+         posts: state.posts.filter((post) => post._id !== action.payload),
+       };
       default:
         return state;
     }
