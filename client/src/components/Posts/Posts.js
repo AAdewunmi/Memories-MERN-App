c9e86423906f0d163b986fa4148e78ffca07c13b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-06 08:38:31 +0000

    Two-Hundred-Thirty-Two Commit: Refactor Posts() function to use isLoading state

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,9 +6,12 @@ import useStyles from "./styles";
 
 
 const Posts = ({ setCurrentId }) => {
-  const { posts } = useSelector((state) => state.posts);
+  const { posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
-  return !posts?.length ? (
+  if (!posts.length && !isLoading) {
+      return 'No Posts!';
+  }
+  return isLoading ? (
     <CircularProgress></CircularProgress>
   ) : (
     <Grid
