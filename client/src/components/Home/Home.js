92c51944e1506b6d6bc506d050e27865b55004cd
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-06 08:46:39 +0000

    Two-Hundred-Thirty-Three Commit: Refactor pagination look and feel

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -90,9 +90,11 @@ const Home = () => {
                 </Button>
               </AppBar>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
-              <Paper elevation={6}>
-                <Paginate page={page}/>
-              </Paper>
+              {!searchQuery && !tags.length && (
+                <Paper elevation={6} className={classes.Paginate}>
+                  <Paginate page={page} />
+                </Paper>
+              )}
             </Grid>
           </Grid>
         </Container>
