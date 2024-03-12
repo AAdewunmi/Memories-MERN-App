8b5f3d8d93e37b59b85a83777e46615f8f230e89
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-12 09:02:28 +0000

    Two-Hundred-Forty-Two Commit: Implement getPost() action function

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,5 +1,27 @@
-import { CREATE, UPDATE, DELETE, FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, END_LOADING} from '../constants/actionTypes';
+import {
+  CREATE,
+  UPDATE,
+  DELETE,
+  FETCH_POST,
+  FETCH_ALL,
+  FETCH_BY_SEARCH,
+  START_LOADING,
+  END_LOADING,
+} from "../constants/actionTypes";
 import * as api from '../api';
+
+export const getPost = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+
+    const { data } = await api.fetchPost(id);
+
+    dispatch({ type: FETCH_POST, payload: { post: data } });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getPosts = (page) => async (dispatch) => {
   try {
