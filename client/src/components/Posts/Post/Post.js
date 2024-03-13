fac43d6fdc890bd87d1e7c2a27d35ad2ffc3ef07
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-13 13:10:36 +0000

    Two-Hundred-Forty-Eight: Fix loading posts on homepage issue

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {Card, CardActions, CardContent, Button, Typography, ButtonBase } from '@material-ui/core';
+import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import ButtonBase from "@mui/material/ButtonBase";
 import CardMedia from "@material-ui/core/CardMedia";
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
@@ -16,6 +17,7 @@ const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("profile"));
+  
   const Likes = () => {
     if (post.likes.length > 0) {
       return post.likes.find(
@@ -43,20 +45,29 @@ const Post = ({ post, setCurrentId }) => {
       </>
     );
   };
+
   const openPost = () => navigate(`/posts/${post._id}`);
 
   return (
     <Card className={classes.card} raised elevation={6}>
-      <ButtonBase className={classes.cardActions} onClick={openPost}>
+      <ButtonBase
+        component="span"
+        name="test"
+        className={classes.cardActions}
+        onClick={openPost}
+      >
+        <CardContent>
           <CardMedia
-            className={classes.media}
+            //className={classes.media}
+            component="img"
+            height="200"
             image={
               post.selectedFile ||
               "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
             }
             title={post.title}
-            component="div"
-          ></CardMedia>
+          />
+
           <div className={classes.overlay}>
             <Typography variant="h6">{post.name}</Typography>
             <Typography variant="body2">
@@ -65,11 +76,14 @@ const Post = ({ post, setCurrentId }) => {
           </div>
           {(user?.result?.googleId === post?.creator ||
             user?.result?._id === post?.creator) && (
-            <div className={classes.overlay2}>
+            <div className={classes.overlay2} name="edit">
               <Button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setCurrentId(post._id);
+                }}
                 style={{ color: "white" }}
                 size="small"
-                onClick={() => setCurrentId(post._id)}
               >
                 <MoreHorizIcon fontSize="medium" />
               </Button>
@@ -88,12 +102,10 @@ const Post = ({ post, setCurrentId }) => {
           >
             {post.title}
           </Typography>
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {post.message}
-            </Typography>
-          </CardContent>
-
+          <Typography variant="body2" color="textSecondary" component="p">
+            {post.message.split(" ").splice(0, 20).join(" ")}...
+          </Typography>
+        </CardContent>
       </ButtonBase>
 
       <CardActions className={classes.cardActions}>
@@ -112,12 +124,13 @@ const Post = ({ post, setCurrentId }) => {
             color="secondary"
             onClick={() => dispatch(deletePost(post._id))}
           >
-            <DeleteIcon fontSize="small" /> Delete
+            <DeleteIcon fontSize="small" /> &nbsp; Delete
           </Button>
         )}
       </CardActions>
     </Card>
   );
+
 };
 
 export default Post;
