38af133a404ee3eb7b806fe4e47042d0bd0b7d86
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-18 09:21:24 +0000

    Two-Hundred-Fifty-Five Commit: Implement skeleton handleComment() function

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -3,11 +3,54 @@ import { Typography, TextField, Button } from "@material-ui/core/";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
 
-const CommentSection = () => {
-    console.log('Comment Section');
-    return(
-        <h1>Comment Section</h1>
-    )
+const CommentSection = ({ post }) => {
+    console.log(post);
+    const classes = useStyles();
+    const [comments, setComments] = useState([1,2,3,4]);
+    const [comment, setComment] = useState('');
+    const handleComment = async () => {
+      
+    };
+    return (
+      <div>
+        <div className={classes.commentsOuterContainer}>
+          <div className={classes.commentsInnerContainer}>
+            <Typography gutterBottom variant="h6">
+              Comments
+            </Typography>
+            {comments.map((c, i) => (
+              <Typography key={i} gutterBottom variant="subtitle1">
+                Comment {i}
+              </Typography>
+            ))}
+          </div>
+          <div style={{ width: "70%" }}>
+            <Typography gutterBottom variant="h6">
+              Write a Comment
+            </Typography>
+            <TextField
+              fullWidth
+              rows={4}
+              variant="outlined"
+              label="Comment"
+              multiline
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <Button
+              style={{ marginTop: "10px" }}
+              fullWidth
+              disabled={!comment.length}
+              color="primary"
+              variant="contained"
+              onClick={handleComment}
+            >
+              Comment
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
 };
 
 export default CommentSection;
