743739b60e3d8173f2aa4da3643b5df25e8b1c8b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-03-20 13:29:26 +0000

    Two-Hundred-Sixty-One Commit: Implement commentPost controller

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from "react";
 import { Typography, TextField, Button } from "@material-ui/core/";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
-import { commentPost } from '../../actions';
+import { commentPost } from "../../actions/posts";
 
 const CommentSection = ({ post }) => {
     const classes = useStyles();
